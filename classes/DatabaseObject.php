a815e4ed8fb63b0140fd80b4ec874018b48a91ee
noakathrein <noakathrein@tsn.at>
2026-01-19 08:10:54 +0100

    jetzt habe ich eine das interface databaseobjecht gemacht, hier werden die getter und delet gemacht

--- classes/DatabaseObject.php
+++ classes/DatabaseObject.php
@@ -1,6 +1,11 @@
 <?php
 
-class DatabaseObject
+interface DatabaseObject
 {
+    public function save(): bool;
+
+    public static function getById( $id);
+    public static function getAll(): array;
+    public static function delete():bool;
 
 }
